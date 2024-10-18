76e3b40e605f914029dd23fd9e9add1074ce3953
Mohammad Sultani <132396736+s7shakib@users.noreply.github.com>
2024-10-18 09:37:09 -0400

    Update hooks.ts

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,5 +1,5 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import type { RooState, AppDispatch } from "./store";
-
+// new change 
 export const useAppDispatch = () => useDispatch <AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RooState> = useSelector;
